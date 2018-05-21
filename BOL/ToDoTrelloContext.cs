5d1db2e55a4b7e4f6e52183a4491c43c5ffe7c5b
Alex Khatuntsev <khatun3000@gmail.com>
2018-05-21 18:56:38 +0300

    AddContext

---
+++ BOL/ToDoTrelloContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BOL.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BOL
+{
+    public class ToDoTrelloContext : DbContext
+    {
+        public ToDoTrelloContext(DbContextOptions<ToDoTrelloContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Task> Tasks { get; set; }
+
+        public DbSet<SubStatus> SubStatuses { get; set; }
+
+        public DbSet<Subscribe> Subscribes { get; set; }
+
+        public DbSet<Role> Roles { get; set; }
+
+        public DbSet<Project> Projects { get; set; }
+
+        public  DbSet<Priority> Priorities { get; set; }
+    }
+}
