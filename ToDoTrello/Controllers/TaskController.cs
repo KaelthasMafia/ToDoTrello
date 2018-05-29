b71b6715e5010850530026b872da20d6787039f3
Alex Khatuntsev <khatun3000@gmail.com>
2018-05-29 18:44:20 +0300

    Add Task list

---
+++ ToDoTrello/Controllers/TaskController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BOL;
+using BOL.Models;
+using Microsoft.AspNetCore.Mvc;
+using Task = BOL.Models.Task;
+
+namespace ToDoTrello.Controllers
+{
+    public class TaskController : BaseController
+    {
+        public TaskController(ToDoTrelloContext context) : base(context)
+        { }
+
+        [HttpGet]
+        public IActionResult TasksList()
+        {
+            List<Task> tasks = db.TaskDb.Get().Where(x=>x.User.Email == User.Identity.Name).ToList();
+            if (tasks.Count != 0)
+            {
+                foreach (var task in tasks)
+                {
+                    int priorityId = db.TaskDb.Get(x => x.TaskId == task.TaskId).FirstOrDefault().TaskId;
+                    task.Priority = db.PriorityDb.Get(x => x.PriorityId == priorityId).FirstOrDefault();
+                }
+
+                ViewBag.Count = "";
+            }
+            ViewBag.Count = "notnull";
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult DeleteTask(int id)
+        {
+            return RedirectToAction("TasksList");
+        }
+
+        [HttpGet]
+        public IActionResult CreateTask()
+        {
+            return View();
+        }
+    }
+}
