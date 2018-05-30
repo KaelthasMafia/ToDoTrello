b73715fa6dbd5f53658faf123f62e46be2fbff20
Alex Khatuntsev <khatun3000@gmail.com>
2018-05-30 11:10:55 +0300

    Create UserController

---
+++ ToDoTrello/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BOL;
+using BOL.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoTrello.Controllers
+{
+    public class UserController : BaseController
+    {
+        public UserController(ToDoTrelloContext context) : base(context)
+        { }
+
+        [HttpGet]
+        public IActionResult ViewProfile()
+        {
+            User user = db.UserDb.Get(x => x.Email == User.Identity.Name).FirstOrDefault();
+            return View(user);
+        }
+    }
+}
